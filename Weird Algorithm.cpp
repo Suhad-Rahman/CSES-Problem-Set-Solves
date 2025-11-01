7588f3ef69fabb2202e70d6f8ba2bc306cbe6d53
Suhad Rahman <suhadturan03@gmail.com>
2025-11-01 14:57:05 +0600

    Weird Algorithm || Collatz Conjecture || (3n+1) Problem
    Link: https://cses.fi/problemset/task/1068
    
    This C++ code implements an algorithm known as the Collatz Conjecture (sometimes called the 3n+1 problem).

---
+++ Weird Algorithm.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    
+    long long n;
+    cin>>n;
+    
+    cout<<n<<" ";
+    
+    while(n!=1){
+        
+        if((n&1)==0){
+            n = n/2;
+        }
+        
+        else{
+            n = (n*3)+1;
+        }
+        cout<<n<<" ";
+        
+    }
+    
+    
+    
+    
+    return 0;
+}
